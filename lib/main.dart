c6d1021766b69edd3570582b62512044dda470ec
Wassy | Ryoga Washizawa <74349349+wassy310@users.noreply.github.com>
2023-11-11 11:06:27 +0900

    Update: Base UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
+void main() {
+  initializeDateFormatting('ja_JP');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme:
+            GoogleFonts.sawarabiGothicTextTheme(Theme.of(context).textTheme),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFF53a697),
+          ),
+          body: const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
