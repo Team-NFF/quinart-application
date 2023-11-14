d711bfa6c60d0e70a69ff66abea7f18ec865950e
Wassy | Ryoga Washizawa <74349349+wassy310@users.noreply.github.com>
2023-11-14 19:17:04 +0900

    Create: Added admin page

---
+++ lib/view/home_page/admin_home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:quinart/component/home_page/drawer/drawer.dart';
+import 'package:quinart/component/home_page/header/header.dart';
+import 'package:quinart/component/appbar/home_appbar.dart';
+import 'package:quinart/component/home_page/main_button/health_check.dart';
+import 'package:quinart/component/home_page/main_button/map.dart';
+import 'package:quinart/component/home_page/main_button/call.dart';
+import 'package:quinart/component/divider/green_divider.dart';
+import 'package:quinart/component/footer/footer.dart';
+
+class AdminHomeScreen extends StatelessWidget {
+  const AdminHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const HomeAppBar(),
+      endDrawer: const HomePageDrawer(),
+      body: const Stack(
+        children: [
+          HomePageHeader(),
+          MapButton(
+            top: 220,
+            left: 20,
+            right: 240,
+          ),
+          HealthCheckButton(),
+          CallButton(
+            top: 220,
+            left: 240,
+            right: 20,
+          ),
+          Positioned(
+            top: 335,
+            left: 0,
+            right: 0,
+            child: GreenDivider(),
+          ),
+        ],
+      ),
+      bottomNavigationBar: const HomePageFooter(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xFF63a697),
+        child: const Icon(Icons.home),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
