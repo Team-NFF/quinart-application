d1d657edb629f3db4410c66db42b34955f5b9488
Wassy | Ryoga Washizawa <74349349+wassy310@users.noreply.github.com>
2024-01-17 13:44:00 +0900

    Update: Separate user pages

--- lib/view/home_page/home.dart
+++ lib/view/home_page/home.dart
@@ -20,17 +20,17 @@ class HomeScreen extends StatelessWidget {
         children: [
           HomePageHeader(),
           MapButton(
-            top: 220,
+            top: 235,
             left: 20,
             right: 170,
           ),
           CallButton(
-            top: 220,
+            top: 235,
             left: 170,
             right: 20,
           ),
           Positioned(
-            top: 335,
+            top: 345,
             left: 0,
             right: 0,
             child: GreenDivider(),
